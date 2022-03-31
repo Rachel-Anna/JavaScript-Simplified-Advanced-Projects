ca071ddcb0dbe4bcb8910a0c27980dfd345a0d7c
Rochie424 <67237482+Rochie424@users.noreply.github.com>
2022-03-31 12:32:27 +0200

    calc complete

--- 27-28-calculator/before/script.js
+++ 27-28-calculator/before/script.js
@@ -24,6 +24,8 @@ What could be a class? calc
 
 */
 
+let storageKey = "sessionStorageCalcKey";
+
 const MULTIPLY_DIVIDE_REGEX =
   /(?<operand1>\S+)\s*(?<operation>[\/\*])\s*(?<operand2>\S+)/;
 const EXPONENT_REGEX = /(?<operand1>\S+)\s*(?<operation>\^)\s*(?<operand2>\S+)/;
@@ -33,27 +35,40 @@ const ADD_SUBTRACT_REGEX =
 const element = document.querySelector("[data-calc]");
 
 class Calc {
-  static equationString = [];
+  static equationArray = [];
 
   static getInput() {}
 
   static globalListener() {
     element.addEventListener("click", (e) => {
-      if (
-        e.target.matches("[data-operation]") ||
-        e.target.matches("[data-number]")
+      if (e.target.matches("[data-number]")) {
+        this.equationArray.push(e.target.innerText);
+        console.log(this.equationArray);
+        Display.show(e.target.innerText);
+      } else if (
+        e.target.matches("[data-operation]") &&
+        this.equationArray.length !== 0
       ) {
-        this.equationString.push(e.target.innerText);
+        this.equationArray.push(e.target.innerText);
+        console.log(this.equationArray);
+      } else if (
+        e.target.matches("[data-operation]") &&
+        e.target.innerText === "-"
+      ) {
+        this.equationArray.push(e.target.innerText);
+        console.log(this.equationArray);
+      } else if (e.target.matches("[data-delete]")) {
+        this.equationArray.pop();
+        console.log(this.equationArray);
+      } else if (e.target.matches("[data-all-clear]")) {
+        this.equationArray.splice(0, this.equationArray.length);
+        console.log(this.equationArray);
       } else if (e.target.matches("[data-equals]")) {
-        let stringForm = this.equationString.toString().replace(/,/g, "");
-        this.performCalc(stringForm);
+        let equationString = this.equationArray.toString().replace(/,/g, "");
+        this.performCalc(equationString);
       }
     });
   }
-
-  static delete() {}
-
-  static allclear() {}
 
   static handleMath({ operand1, operand2, operation }) {
     const number1 = parseFloat(operand1);
@@ -94,3 +109,15 @@ class Calc {
 }
 
 Calc.globalListener();
+
+const output = document.querySelector("[data-output]");
+const history = document.querySelector("[data-history]");
+
+class Display {
+  static show(input) {
+    output.innerText = input;
+  }
+
+  static allClear() {}
+  static delete() {}
+}
